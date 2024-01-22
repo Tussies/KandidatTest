7b5e47ad76ffd94eb96b2967bbea187213f39708
L3B0W5K1 <74051842+L3B0W5K1@users.noreply.github.com>
2024-01-22 20:12:05 +0100

    Update manuellkarta.py
    jox

--- manuellkarta.py
+++ manuellkarta.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 
 # Constants
 WIDTH, HEIGHT = 25, 25
@@ -12,12 +11,12 @@ PURPLE = 2  # Barrier
 RED = 3  # Stairs
 
 def create_empty_map():
-    return [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+    return [[0 for _ in range(HEIGHT)] for _ in range(WIDTH)]
 
 def draw_map(screen, game_map):
     for x in range(WIDTH):
         for y in range(HEIGHT):
-            tile = game_map[y][x]
+            tile = game_map[x][y]
             if tile == WHITE:
                 color = (255, 255, 255)
             elif tile == BLACK:
@@ -37,12 +36,12 @@ def main():
     game_map = create_empty_map()
 
     # Manually set some pixels
-    for x in range(25):
-        game_map[0][x] = BLACK
-        game_map[x][0] = BLACK
-        game_map[x][24] = BLACK
-        game_map[24][x] = BLACK
-        game_map[12][x] = BLACK
+    for y in range(25):
+        game_map[0][y] = BLACK
+        game_map[y][0] = BLACK
+        game_map[y][24] = BLACK
+        game_map[24][y] = BLACK
+        game_map[12][y] = BLACK
     
     game_map[11][20] = BLACK
     game_map[11][22] = BLACK
@@ -52,17 +51,19 @@ def main():
     game_map[23][22] = BLACK
     game_map[23][21] = RED
 
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                running = False
 
         screen.fill((255, 255, 255))  # Set background color to white
         draw_map(screen, game_map)
         pygame.display.flip()
 
         pygame.time.Clock().tick(30)  # Limit the frame rate to 30 FPS
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
